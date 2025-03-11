163a49ba3d985fd871fe749398074aa81b95f8de
guilluy <baptisteguilluy0728@gmail.com>
2025-03-11 13:20:32 +0100

    test

---
+++ TCLab_software_V4/package12.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize
+from matplotlib import colors as mcolors
+
+import package_DBR
+from package_DBR import myRound, SelectPath_RT, Delay_RT, FO_RT, FOPDT, SOPDT, FOPDT_cost, SOPDT_cost, Process, Bode
+
+
+
+
+# Variable from graphical method 
+
+a = (0.529-0.367)#temp dif
+Kp = 0.529 #without unit
+T1 = 98 #sec
+T2 = 120 #sec
+Tu = 45 #sec
+Tg = 170 #sec
+
+MV = []
+
+
+def broida1(MV,Kp,Tg,Tu,Ts): 
+    PV = FOPDT(MV,Kp,Tg,Tu,Ts)
+
+
+def broida2(MV,Kp,T2,T1,Ts): 
+    PV = FOPDT(MV,Kp,5.5*(T2-T1),(2.8*T1)-(1.8*T2),Ts)
